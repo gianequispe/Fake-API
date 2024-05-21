b0f71a8d2e28978856e60efb6b6e312a393bff1a
gianequispe <giane132000@gmail.com>
2024-05-21 18:07:41 -0500

    Add cors

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const cors = require('cors')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
+
+server.use(cors());
+
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/product/:resource/:id/show': '/:resource/:id'
